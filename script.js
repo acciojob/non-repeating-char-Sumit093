a0cdc7c0239d88c66e14452bc0f19172e990abbc
Sumit Haral <146067244+Sumit093@users.noreply.github.com>
2025-10-17 12:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	let n = str.length;
+	if(n==0){
+		return "null";
+	}
 	for(int j=0;j<n;j++){
 		let ch = str[j];
 		if(str.indexOf(ch) === str.lastIndexOf(ch)){
